5874d89e1a827a6750cb3c81575928d44c9ffd1e
hannabh <hanna.b.harding@gmail.com>
2018-11-21 22:52:25 +0000

    imported matplotlib and tested printing data

--- import_data.py
+++ import_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
+import matplotlib as plot
 
-data = pd.read_csv("gender_development.csv")
-print(data.head)
+data = pd.read_csv("gender_development.csv", index_col = 1)
+print(data.head(5))
 
-
+print(data.loc["Germany", "Expected Years of Education (Female)"])
