01c10bb2205cd6f845cedeaed1e05f662f7aa2b0
hannabh <hanna.b.harding@gmail.com>
2018-11-28 19:33:06 +0000

    trying stuff out...

--- import_data.py
+++ import_data.py
@@ -1,19 +1,23 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import json
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
 
+app = Flask(__name__)
 
 @app.route('/')
 def analysis():
-    full_data = pd.read_csv("gender_development.csv", index_col=1)
-    return str(full_data)
+    raw_data = pd.read_csv("gender_development.csv", index_col=1)
+    list = data.index
+    countries = list.T.todict().values()
+    full_data = raw_data.T.to_dict().values()
+    return render_template("homepage_page.html", countries = countries)
 
 app.run(debug=True)
 
-with open('data.json', 'w') as outfile:
-    json.dump(full_data, outfile)
+#countries: list of strings,
+#info: dictionary or lots of arguments
+
 
 # data = pd.read_csv("gender_development.csv", index_col = 1)
 # print(data.head(5))
