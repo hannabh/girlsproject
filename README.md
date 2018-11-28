c4c593391b06c20137c70d3f3794e3c78ee95720
hannabh <hanna.b.harding@gmail.com>
2018-11-28 23:06:10 +0000

    Updated README with project aim and descriptions of files

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # girlsproject
+
+###Aim  
+To create a webpage with a list of country names, when you click a country it takes you to a new webpage with some statistics for that country. (Stats from kaggle dataset)
+
+###Files 
+dataset.csv - *dataset with country names and information, downloaded from kaggle*  
+coding.py - *main python code*  
+homepage.html - *webpage with list of countries*  
+info_page.html - *webpage with country information*  
+requirements.txt - *what you need to have installed in order for the code to run (ignore this file)*  
+hannas_testing_space - *Hanna trying out the pandas library to do data analysis (ignore this file too)*  
--- coding.py
+++ coding.py
@@ -5,16 +5,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage_function():
-    raw_data = pd.read_csv("gender_development.csv", index_col=1)
+    raw_data = pd.read_csv("dataset.csv", index_col=1)
     list = raw_data.index
     #convert list to list of strings called country_list
     return render_template("homepage.html", countries = country_list)
 
 def data_page_function():
-    raw_data = pd.read_csv("gender_development.csv", index_col=1)
+    raw_data = pd.read_csv("dataset.csv", index_col=1)
     full_data = raw_data.T.to_dict().values()
     # country_data: should be dictionary with info about 1 country
-    return render_template("data_page.html", country_info = country_data)
+    return render_template("info_page.html", country_info = country_data)
 
 app.run(debug=True)
 
