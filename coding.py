d4b4d1f560976f61bd07e8e678cc4182302043e7
hannabh <hanna.b.harding@gmail.com>
2018-11-29 20:03:23 +0000

    got localhost to work properly

--- coding.py
+++ coding.py
@@ -6,18 +6,24 @@ app = Flask(__name__)
 @app.route('/')
 
 def homepage_function():
-    raw_data = pd.read_csv("dataset.csv", index_col=1)
-    list = raw_data.index
+    #raw_data = pd.read_csv("dataset.csv", index_col=1)
+    #list = raw_data.index
     # country_list: convert list to list of strings
-    return render_template("homepage.html", countries = country_list)
+    return render_template("homepage.html", countries = "country_list")
 
-def data_page_function():
-    raw_data = pd.read_csv("dataset.csv", index_col=1)
-    full_data = raw_data.T.to_dict().values()
+
+@app.route("/<country>/")
+
+def info_page_function(country):
+    return render_template("info_page.html", country_info = country)
+    # raw_data = pd.read_csv("dataset.csv", index_col=1)
+    # full_data = raw_data.T.to_dict().values()
     # country_data: dictionary with info about 1 country
-    return render_template("info_page.html", country_info = country_data)
+    # return render_template("info_page.html", country_info = country_data)
+
 
 app.run(debug=True)
+
 
 
 #Note: return str(full_data) prints all the data to localhost
